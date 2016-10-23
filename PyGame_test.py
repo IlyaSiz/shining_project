de9d2b73ae218592d97fa950659e51ff990c235f
Ilya Siz <ilyasiz@gmail.com>
2016-10-23 15:14:24 +0300

    Первоначальное начало игрушки

---
+++ PyGame_test.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Импортируем библиотеку pygame
+import pygame, sys
+from pygame import *
+from player import Player
+from blocks import *
+
+# Объявляем переменные
+WIN_WIDTH = 800  # Ширина создаваемого окна
+WIN_HEIGHT = 640  # Высота
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
+BACKGROUND_COLOR = "#444444" #"#004400"
+
+def main():
+    hero = Player(55, 55)  # создаем героя по (x,y) координатам
+    left = right = False  # по умолчанию — стоим
+    up = False
+    entities = pygame.sprite.Group()  # Все объекты
+    platforms = []  # то, во что мы будем врезаться или опираться
+    entities.add(hero)
+    level = [
+        "-------------------------",
+        "-                       -",
+        "-                       -",
+        "-                       -",
+        "-            --         -",
+        "-                       -",
+        "--                      -",
+        "-                       -",
+        "-                   --- -",
+        "-                       -",
+        "-                       -",
+        "-      ---              -",
+        "-                       -",
+        "-   -----------         -",
+        "-                       -",
+        "-                -      -",
+        "-                   --  -",
+        "-                       -",
+        "-                       -",
+        "-------------------------"]
+    x = y = 0  # координаты
+    for row in level:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
+            x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
+
+    timer = pygame.time.Clock()
+    #pygame.init()  # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
+    pygame.display.set_caption("Shining Force New")  # Пишем в шапку
+    bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
+    # будем использовать как фон
+    bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
+
+    done = True
+
+    while done:  # Основной цикл программы
+        timer.tick(60)
+        for e in pygame.event.get():  # Обрабатываем события
+            if e.type == pygame.QUIT:
+                done = False
+                #pygame.quit()
+                #sys.exit()
+                #raise(SystemExit, "QUIT")
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
+                left = True
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
+                right = True
+
+            if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
+                right = False
+            if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
+                left = False
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
+                up = True
+            if e.type == pygame.KEYUP and e.key == pygame.K_UP:
+                up = False
+        screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
+
+        hero.update(left, right, up, platforms)  # передвижение
+        entities.draw(screen)  # отображение всего
+        pygame.display.update()  # обновление и вывод всех изменений на экран
+        #pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
