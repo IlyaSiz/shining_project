12fa6d70076cc20e724440077a16a2d803286bad
Ilya Siz <ilyasiz@gmail.com>
2016-10-23 21:49:38 +0300

    Добавлена камера

--- PyGame_test.py
+++ PyGame_test.py
@@ -6,12 +6,13 @@ import pygame, sys
 from pygame.locals import *
 from player import Player
 from blocks import *
+from camera import Camera
 
 # Объявляем переменные
 WIN_WIDTH = 800  # Ширина создаваемого окна
 WIN_HEIGHT = 640  # Высота
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
-BACKGROUND_COLOR = "#333333" #"#004400"
+BACKGROUND_COLOR = "#222222" #"#004400"
 
 def main():
     hero = Player(55, 55)  # создаем героя по (x,y) координатам
@@ -41,6 +42,32 @@ def main():
         "-                       -",
         "-                       -",
         "-------------------------"]
+    level = [
+        "----------------------------------",
+        "-                                -",
+        "-                                -",
+        "-                                -",
+        "-            --                  -",
+        "-                        --      -",
+        "--                               -",
+        "-                                -",
+        "-                   ----     --  -",
+        "-                                -",
+        "--     -                         -",
+        "-      -                         -",
+        "-                            --- -",
+        "-                                -",
+        "-                                -",
+        "-     ----                       -",
+        "-                                -",
+        "-                   ----         -",
+        "-   -------                      -",
+        "-                         -      -",
+        "-                            --  -",
+        "-                                -",
+        "-                                -",
+        "----------------------------------"]
+
     x = y = 0  # координаты
     for row in level:  # вся строка
         for col in row:  # каждый символ
@@ -55,10 +82,14 @@ def main():
     timer = pygame.time.Clock()
     #pygame.init()  # Инициация PyGame, обязательная строчка
     screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
-    pygame.display.set_caption("Shining Force New")  # Пишем в шапку
+    pygame.display.set_caption("Super Mario")  # Пишем в шапку
     bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
     # будем использовать как фон
     bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
+
+    total_level_width = len(level[0]) * PLATFORM_WIDTH  # Высчитываем фактическую ширину уровня
+    total_level_height = len(level) * PLATFORM_HEIGHT  # высоту
+    camera = Camera(camera_configure, total_level_width, total_level_height)
 
     mainLoop = True
 
@@ -86,9 +117,25 @@ def main():
         screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
 
         hero.update(left, right, up, platforms)  # передвижение
-        entities.draw(screen)  # отображение всего
+        #entities.draw(screen)  # отображение всего -- до введения камеры
+        camera.update(hero)  # центризируем камеру относительно персонажа
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
         pygame.display.update()  # обновление и вывод всех изменений на экран
         #pygame.display.flip()
+
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l+WIN_WIDTH / 2, -t+WIN_HEIGHT / 2
+
+    l = min(0, l)                           # Не движемся дальше левой границы
+    l = max(-(camera.width-WIN_WIDTH), l)   # Не движемся дальше правой границы
+    t = max(-(camera.height-WIN_HEIGHT), t) # Не движемся дальше нижней границы
+    t = min(0, t)                           # Не движемся дальше верхней границы
+    return Rect(l, t, w, h)
+
 
 if __name__ == "__main__":
     main()
